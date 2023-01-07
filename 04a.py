4e753b55c91ca77fa2e0d8c264f5d4eb01cfcbbd
Daniel Nýdrle <nydrledaniel@gmail.com>
2023-01-07 04:01:26 +0100

    04a

---
+++ 04a.py
@@ -0,0 +1,13 @@
+import random
+
+def main():
+    x = random.random()
+
+    if x < 0.8:
+        favourite = "dogs"
+    elif x < 0.9:
+        favourite = "cats"
+    else:
+        favourite = "bats"
+    print("I love " + favourite)
+main()
